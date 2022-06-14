f523b4b233ce7ac4311d99e3a6de7666454d9468
Renato Sampaio <renatosampaio81@yahoo.com.br>
2022-06-14 14:05:18 -0300

    Cap02 - Até Aula23 (completo)

--- Cap02/hello.js
+++ Cap02/hello.js
@@ -1,1 +1,13 @@
-console.log("Hello World !!")
+/*
+Estruturas de Repetição
+*/
+
+var lista_frutas = ['uva', 'morango', 'melancia', 'abacate']
+//console.log(typeof(lista_frutas))
+
+var i = 0
+
+while (i < lista_frutas.length){
+    console.log(lista_frutas[i])
+    i++
+}
