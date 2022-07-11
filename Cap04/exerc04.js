1c3d66f206d298c98c586b23c8aa2335640ca259
Renato Sampaio <renatosampaio81@yahoo.com.br>
2022-07-11 15:53:10 -0300

    Cap04 - até a Aula 04 (Exercício 04)

--- Cap04/exerc04.js
+++ Cap04/exerc04.js
@@ -2,3 +2,10 @@
 Escreva uma função JS para clonar um array de entrada,
 deve ser gerada uma cópia exata como saída e no formato de array
 */
+
+var array_Clone = function(arr1) {
+    return arr1.slice(0); //retorna um clone (numa nova area de memória) do array de entrada
+}
+
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
--- Cap04/exerc05.js
+++ Cap04/exerc05.js
@@ -1,5 +1,6 @@
 /*
-Escreva um programa JS para encontrar o item mais frequente de um array
+Escreva um programa JS para encontrar o item mais frequente
+de um array.
 
 Array de entrada = var arr1 = [3, 'a', 'a', 'a', 2, 3, 'a',3 , 'a', 2, 4, 9, 3]
 Saída Esperada = letra a (5 vezes)
