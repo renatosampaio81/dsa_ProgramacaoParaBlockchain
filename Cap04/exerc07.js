fc78efb378525fae7c38a7383b30e15285ee7f47
Renato Sampaio <renatosampaio81@yahoo.com.br>
2022-07-12 20:41:31 -0300

    Cap04 - até a Aula 05 (Exercício 07)

--- Cap04/exerc07.js
+++ Cap04/exerc07.js
@@ -9,3 +9,16 @@ var estudante = {
 
 Saída esperada: Aluno DSA, Blockchain, 180
 */
+
+// Minha Solução:
+let estudante = {
+    nome: "Aluno DSA",
+    turma: "Blockchain",
+    codigo: 180
+}
+
+console.log (estudante.nome + ', ' + estudante.turma + ', ' + estudante.codigo);
+
+// Solução do Professor
+const forOwn = (obj, fn) => Object.keys(obj).forEach(key => fn(obj[key], key, obj));
+forOwn({ Nome: 'Aluno DSA', turma: 'Blockchain', codigo: 180 }, v => console.log(v));
