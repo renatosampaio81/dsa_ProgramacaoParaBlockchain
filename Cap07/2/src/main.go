a1ef8822d71239d37da3c59b583a8d6c27b924a6
Renato Sampaio <renatosampaio81@yahoo.com.br>
2022-09-13 14:23:15 -0300

    Cap07 - até a Aula 06 (Declaracao de Variaveis e Tipos Primitivos)

---
+++ Cap07/2/src/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	var num int
+	num = 100
+
+	println(num)
+
+	var num2 float32 = 25.
+	println(num2)
+
+	palavra := "Teste"
+	println(palavra)
+
+	myComplex := complex(2,3)
+	println(myComplex)
+	println(real(myComplex))
+	println(imag(myComplex))
+
+}
