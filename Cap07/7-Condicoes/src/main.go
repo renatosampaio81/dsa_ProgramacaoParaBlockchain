fdc624917049305a8cce41e78e7ca0417e2862ef
Renato Sampaio Albuquerque <renatosampaio81@yahoo.com.br>
2023-02-07 17:21:43 -0500

    Cap07 - até a Aula 13 (Trabalhando com Estruturas Condicionais)

---
+++ Cap07/7-Condicoes/src/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main()  {
+
+	num := 5
+
+	switch {
+		
+	case num == 1:
+		println("Num igual a 1")
+	case num > 1:	
+		println("Num diferente a 1")
+	}
+
+	/*
+	if num == 1 {
+		println("Num igual a 1")
+	} else {
+		println("Num diferente a 1")
+	}
+	*/
+
+}
