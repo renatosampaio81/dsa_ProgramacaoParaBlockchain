562b04c31fb06499fd910c69e15e8cfed44a4c12
Renato Sampaio Albuquerque <renatosampaio81@yahoo.com.br>
2023-02-07 17:05:05 -0500

    Cap07 - até a Aula 11 (Trabalhando com Operadores Aritméticos)

---
+++ Cap07/6-OperMath/src/main.go
@@ -0,0 +1,22 @@
+package main
+
+func main()  {
+
+	soma := 3 + 6
+	println(soma)
+
+	sub := 6 - 3
+	println(sub)
+
+	resto := 5 % 2
+	println(resto)
+
+	num := 1
+	num++
+	println(num)
+
+	num *= 9
+	println(num)
+	
+}
+
